82fa8a2d4f35757c87c3ad44c05ee86fb563eeac
Gus <gsc13@tuta.io>
2021-05-12 19:48:29 -0300

    Add Scripts for About, Contact, Footer and Menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './assets/stylesheet/style.css';
 import './assets/images/bg-m3.jpg';
-import './assets/images/map.png';
 
 import renderNavigation from './scripts/navbar';
 import { mainContainer, homeContent } from './scripts/home';
+import renderMenu from './scripts/menu';
+import renderAbout from './scripts/about';
+import renderContact from './scripts/contact';
+import renderFooter from './scripts/footer';
 
 const loadPage = () => {
   const content = document.querySelector('#content');
@@ -11,8 +14,9 @@ const loadPage = () => {
 
   content.appendChild(renderNavigation());
 
-  homeContent(main);
+  renderContact(main);
   content.appendChild(main);
+  content.appendChild(renderFooter());
 };
 
 loadPage();
---
+++ src/scripts/footer.js
@@ -0,0 +1,18 @@
+const renderFooter = () => {
+  const footerContainer = document.createElement('footer');
+  const paragraph = document.createElement('p');
+  const link1 = document.createElement('a');
+  const link2 = document.createElement('a');
+
+  link1.href = 'https://unsplash.com/@lindsaymoe?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText';
+  link1.textContent = 'Lindsay Moe';
+
+  link2.href = 'https://unsplash.com/@lindsaymoe?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText';
+  link2.textContent = 'Unsplash';
+
+  paragraph.append('Photo by', link1, ' on ', link2);
+  footerContainer.appendChild(paragraph);
+  return footerContainer;
+};
+
+export default renderFooter;
